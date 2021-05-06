e767defac69db0c6876a3c12cd077cd5885026f5
Hermann Ritzenthaler <hmr@riki5.de>
2021-05-06 20:09:25 +0200

    Initial Commit

---
+++ src/main/java/NetworkPlannerApplication.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class NetworkPlannerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(NetworkPlannerApplication.class, args);
+
+    }
+}
