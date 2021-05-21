256617f680a94d76fa04f7d3aa5a84aacf51067e
alimangal <84561530+alimangal@users.noreply.github.com>
2021-05-21 10:12:39 +0200

    add comment

--- src/main/java/service/NetworkplanCalculator.java
+++ src/main/java/service/NetworkplanCalculator.java
@@ -5,6 +5,7 @@ public final class NetworkplanCalculator {
     public static void fillNetworkplan(Networkplan networkplan) {
         // Calculate Networkplan
         // This is a new commit
+        // This is Mangal
     }
 
 
