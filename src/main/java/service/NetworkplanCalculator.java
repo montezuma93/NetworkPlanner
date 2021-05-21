c69b20534c1a92287f529f72e2c20590606955dc
Rynbor <nikki1402@gmx.de>
2021-05-21 10:25:16 +0200

    Neuer Kommentar

--- src/main/java/service/NetworkplanCalculator.java
+++ src/main/java/service/NetworkplanCalculator.java
@@ -5,6 +5,7 @@ public final class NetworkplanCalculator {
     public static void fillNetworkplan(Networkplan networkplan) {
         // Calculate Networkplan
         // This is a new commit
+        // Hallo hier steht mein neuer Kommentar
     }
 
 
