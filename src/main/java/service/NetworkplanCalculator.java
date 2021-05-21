3ce9233543e05c596133d7564ec6cfb18d66f794
M-A-X-I-M <kplusm@live.de>
2021-05-21 11:22:36 +0200

    2nd comment © Max Becker (M-A-X-I-M)

--- src/main/java/service/NetworkplanCalculator.java
+++ src/main/java/service/NetworkplanCalculator.java
@@ -5,7 +5,8 @@ public final class NetworkplanCalculator {
     public static void fillNetworkplan(Networkplan networkplan) {
         // Calculate Networkplan
         // This is a new commit
-    	// This is my comment © Max Becker (M-A-X-I-M)
+    	// 1st comment © Max Becker (M-A-X-I-M)
+        // 2nd comment © Max Becker (M-A-X-I-M)
     }
 
 
