55fecf543bb064322cce5bcff59af5fe1ef020f1
alimangal <84561530+alimangal@users.noreply.github.com>
2021-05-21 10:12:39 +0200

    add comment

--- src/main/java/service/NetworkplanCalculator.java
+++ src/main/java/service/NetworkplanCalculator.java
@@ -5,12 +5,8 @@ public final class NetworkplanCalculator {
     public static void fillNetworkplan(Networkplan networkplan) {
         // Calculate Networkplan
         // This is a new commit
-
-        //Comment from Alina
+        // This is Mangal
     }
 
-    public int calculateEndDate(int start, int duration) {
-        return start + duration;
-    }
 
 }
