521fc8014cf4e8bcd6c1ad610158e834441d8a3a
montezuma93 <hmr@riki5.de>
2021-06-11 12:31:24 +0200

    adjust networkplan

--- src/main/java/NetworkPlannerApplication.java
+++ src/main/java/NetworkPlannerApplication.java
@@ -10,20 +10,37 @@ import java.util.List;
 @SpringBootApplication
 public class NetworkPlannerApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Assignment createBackend = null;
-        Assignment createFrontend = new Assignment("frontend",10, Arrays.asList(createBackend), Collections.emptyList());
+        Assignment createFrontend = null;
+        createFrontend = new Assignment("frontend",10, Arrays.asList(createBackend), Collections.emptyList());
         createBackend = new Assignment("backend",20, Collections.emptyList(), Arrays.asList(createFrontend));
+
         Networkplan networkplan = new Networkplan(Arrays.asList(createBackend, createFrontend));
+
+
         for (int i = 0; i < networkplan.getAssignments().size(); i++) {
-            System.out.println(networkplan.getAssignments().get(i).name);
-            System.out.println("->.................................................");
-            for (int j = 0; j < networkplan.getAssignments().get(i).getSuccessors().size(); j++) {
-                System.out.println(networkplan.getAssignments().get(i).getSuccessors().get(j).name);
-            }
+            Assignment assignment = networkplan.getAssignments().get(i);
+            System.out.println("----------------------------------------");
+            System.out.println("| "+ assignment.getEarliestStartDate() + "     " + assignment.name + "     " +  assignment.getEarliestEndDate());
+            System.out.println("|           " + networkplan.getAssignments().get(i).getDuration() );
+            System.out.println("| " + assignment.getLatestStartDate() + "                  "  +  assignment.getLatestEndDate());
+            System.out.println("----------------------------------------");
             System.out.println(" ");
         }
         NetworkplanCalculator.fillNetworkplan(networkplan);
+
+
+        System.out.println("CALCULATING ----------------------------------------");
+        for (int i = 0; i < networkplan.getAssignments().size(); i++) {
+            Assignment assignment = networkplan.getAssignments().get(i);
+            System.out.println("----------------------------------------");
+            System.out.println("| "+ assignment.getEarliestStartDate() + "     " + assignment.name + "     " +  assignment.getEarliestEndDate());
+            System.out.println("|           " + networkplan.getAssignments().get(i).getDuration() );
+            System.out.println("| " + assignment.getLatestStartDate() + "                  "  +  assignment.getLatestEndDate());
+            System.out.println("----------------------------------------");
+            System.out.println(" ");
+        }
         List<String> critialPath = NetworkplanCalculator.calculateCriticalPath(networkplan);
 
 
