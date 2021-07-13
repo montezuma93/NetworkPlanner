ec54044acc28f010de5b8fab569ae00cb5708ecd
Alina <anilochka93@gmail.com>
2021-07-13 09:07:16 +0200

    Changes in Schulung

--- src/main/java/Schulung/Professor.java
+++ src/main/java/Schulung/Professor.java
@@ -5,10 +5,9 @@ public class Professor extends Person {
 
     public Professor(String name, String email, String telefonnummer, Adresse adresse, float lohnOut) {
         super(name, email, telefonnummer, adresse);
-        lohnOut = 3400;
         this.lohn = lohnOut;
         Adresse wAdresse5 = new Adresse("Kasernstr.", 12479, "Graz", "Österreich");
-        Professor professor = new Professor("Peter", "email@gmail.com", "48568", wAdresse5, lohnOut);
+        Professor professor = new Professor("Peter", "email@gmail.com", "48568", wAdresse5, 3400);
 
     }
 
